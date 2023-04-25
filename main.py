6f48c8c8ee4d73372fd95ed5508450a9caea765f
Gloria Dias <gloriadias123@gmail.com>
2023-04-25 16:43:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice=input('Now you are in cross road.You have to choose where you want to go:"Left" or "Right"?\n')
+option_left_right=choice.lower()
+# convert_lower_option=option_left_right.lower()
+if(option_left_right=="left"):
+  choice2=input('Do you want to "Swim" or "wait"?\n')
+  choice2_covert=choice2.lower()
+  if(choice2_covert=="wait"):
+    choice3=input("Which door?Red,blue,yellow:\n")
+    choice3_convert=choice3.lower()
+    if(choice3_convert=="red"):
+      print("You are burnt by fire.Game over")
+    elif(choice3_convert=="blue"):
+      print("You are eaten by Beast.Game over")
+    elif(choice3_convert=="yellow"):
+      print("You win!You get the treasure!")
+    else:
+      print("Game over!!")
+  else:
+    print("You are attacked by trout.Game over!!!")
+else:
+  print("Fall into a hole.Game Over!!!")
+
